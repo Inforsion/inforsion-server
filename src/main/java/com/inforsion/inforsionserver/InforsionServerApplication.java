9383c86462f85ad6e88a2e5730f5264d357c3dc2
김민균 <peter_kim@gimmingyun-ui-MacBookAir.local>
2025-08-12 11:45:10 +0900

    Feat:Intial Setting

---
+++ src/main/java/com/inforsion/inforsionserver/InforsionServerApplication.java
@@ -0,0 +1,13 @@
+package com.inforsion.inforsionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InforsionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InforsionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inforsion/inforsionserver/InforsionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inforsion.inforsionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InforsionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
