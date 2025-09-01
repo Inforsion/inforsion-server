ef3478457f48b885e0bb2eb315c9fb21860e5d53
김민균 <151561709+dirak4545@users.noreply.github.com>
2025-09-01 22:09:15 +0900

    Revert "Origin/feat/#14"

--- src/main/java/com/inforsion/inforsionserver/domain/order/controller/OrderController.java
+++
@@ -1,9 +0,0 @@
-package com.inforsion.inforsionserver.domain.order.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class OrderController {
-    // 주문 생성
-
-}
--- src/main/java/com/inforsion/inforsionserver/domain/order/repository/OrderRepositoryCustom.java
+++
@@ -1,5 +0,0 @@
-package com.inforsion.inforsionserver.domain.order.repository;
-
-public interface OrderRepositoryCustom {
-    Long deleteOrder(Integer OrderId);
-}
